8990f26b0e6ec27ef43a6161b611ae722db6485a
RanelShine <ranelleshine076@gmail.com>
2025-04-10 08:36:08 +0100

    verification des modifications

--- app/Models/contact.php
+++ app/Models/contact.php
@@ -2,24 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Symfony\Component\HttpFoundation\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class contact extends Model
+
+class Contact extends Model
 {
     use HasFactory, Notifiable;
-     
-   protected $fillable = [
-        'name',
-        'email',
-        'message',
-        'subject',
-    ];
 
-    public function getRouteKeyName()
-    {
-        return 'name';
-    }
+    protected $fillable = [
+        'name', 
+        'email',
+        'sujet',
+       'message',
+    ];
 }
